a80e678e1e64cb82622c55ce4edd7c7acf85b1f0
unknown <muniyaakter21@gmail.com>
2024-08-15 16:19:02 +0600

    added all product route

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const port = process.env.PORT || 5000
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:5174', 'https://stunning-lebkuchen-fe383d.netlify.app'],
   credentials: true,
-  optionSuccessStatus: 200,
+  optionSuccessStatus: 200, 
 }
 
 
 
 app.use(cors(corsOptions))
-app.use(express.json())
+app.use(express.json()) 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.6irp4bx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6irp4bx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,13 +32,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // const productCollection = client.db('E-commerce').collection('products')
+
+    const database = client.db("E-commerce");
+        const productCollection = database.collection("products");
+
+    app.get('/product', async (req, res) => {
+      const result = await productCollection.find().toArray()
+      res.send(result)
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close(); 
   }
 }
 run().catch(console.dir);
@@ -48,7 +55,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('E-commerce server is running!')
 })
 
 app.listen(port, () => {
